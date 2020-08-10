5e1989c0f49787fea6c8f6b3e92920404bd3ff81
Leo <leoc@sparkamplify.com>
2020-08-10 17:58:48 +0800

    Updates

---
+++ precache-manifest.52d38f903f9f06182f936213c9f1fc4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bae49873b917f89d47491335e7ed27",
+    "url": "/react-hero/index.html"
+  },
+  {
+    "revision": "c353e611cbd951dbb0fa",
+    "url": "/react-hero/static/css/main.6e7a96eb.chunk.css"
+  },
+  {
+    "revision": "7d9f1380ee7f3c413903",
+    "url": "/react-hero/static/js/2.6208149e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hero/static/js/2.6208149e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c353e611cbd951dbb0fa",
+    "url": "/react-hero/static/js/main.1cc02129.chunk.js"
+  },
+  {
+    "revision": "ae02c4c1f7b146dc5125",
+    "url": "/react-hero/static/js/runtime-main.7b798acc.js"
+  }
+]);
